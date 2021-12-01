e37edf372a424cef33c8422b79e1f49e54762aa4
LuK1337 <priv.luk@gmail.com>
2021-12-01 22:44:26 +0100

    Initial commit

---
+++ app/src/main/java/com/luk/owa/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.luk.owa
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val webView by lazy {
+        WebView(this).apply {
+            settings.javaScriptEnabled = true
+            settings.userAgentString =
+                "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.0.0 Mobile Safari/537.36"
+
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+
+                    if (url?.startsWith("${BuildConfig.OWA_HOST}/lm_auth_proxy") == true ||
+                        url?.startsWith("${BuildConfig.OWA_HOST}/owa/auth/logon.aspx") == true
+                    ) {
+                        view?.evaluateJavascript(LOGIN_JS) {}
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(webView)
+
+        webView.loadUrl(BuildConfig.OWA_HOST)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu!!.add(Menu.NONE, 0, Menu.NONE, R.string.menu_refresh).apply {
+            setIcon(android.R.drawable.ic_menu_rotate)
+            setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+            setOnMenuItemClickListener {
+                webView.loadUrl(BuildConfig.OWA_HOST)
+                webView.reload()
+                true
+            }
+        }
+        return true
+    }
+
+    override fun onBackPressed() {}
+
+    companion object {
+        private const val LOGIN_JS = """
+            let logonForm = document.getElementsByName("logonForm")[0] || document.getElementById("logonForm");
+            logonForm.username.value = "${BuildConfig.OWA_USERNAME}";
+            logonForm.password.value = "${BuildConfig.OWA_PASSWORD}";
+            HTMLFormElement.prototype.submit.call(logonForm)
+        """
+    }
+}
